0eefa764a1e5febf102bb309b806a3c0bae80419
Ulises <ulises.argueta95@gmail.com>
2023-08-13 20:34:37 -0500

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as file_name:
+        file_name.write(f"{file_content}")
+    
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a", encoding="utf-8") as file_name:
+        file_name.write(f"{append_content}")
+    
 
 def read_file(file_name):
-    pass
+    return open(f"{file_name}.txt", encoding="utf-8").read()
